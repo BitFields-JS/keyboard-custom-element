838f21edf1f6178974521cbb2819ed07f9ebb892
AES <josiphaboic@gmail.com>
2023-02-16 20:33:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,27 +4,84 @@ class XKeyboard extends HTMLElement {
 		this.style.display = 'flex';
 		this.style.justifyContent = 'center';
 		this.style.alignItems = 'center';
+		this.keyboardPressEvent = new Event('x-keyboard-press', { bubbles: true, cancelable: false });
+		let styles = document.createElement('style');
+		styles.textContent = `
+		.keyboard-panel {
+			display: grid;
+			width: 100%;
+			grid-template-columns: 1fr 1fr 1fr;
+			background: var(--background-0);
+			grid-column-gap: 1rem;
+			grid-row-gap: 1rem;
+		}
 
+		.keyboard-panel p {
+			user-select: none;
+			font-size: var(--x-lg);
+			padding: 2rem;
+			transition: box-shadow 0.5s ease-out;
+		}
+
+		.keyboard-panel p:hover {
+			box-shadow: 0 0 12px -6px var(--blue) inset;
+			transition: all 0.5s ease-in;
+		}
+
+		.keyboard-panel p:active {
+			background: var(--background-1);
+			text-shadow: 0 0 12px var(--blue);
+			transition: all 0.05s ease-in;
+		}
+
+		@media screen and (max-width: 600px) {
+			.keyboard-panel {
+				grid-template-columns: 1fr 1fr 1fr;
+				background: var(--background-0);
+				grid-column-gap: 0.25rem;
+				grid-row-gap: 0.25rem;
+			}
+		
+			.keyboard-panel p {
+				user-select: none;
+				font-size: var(--x-lg);
+				padding: 1rem;
+				transition: box-shadow 0.5s ease-out;
+			}
+		
+			.keyboard-panel p:hover {
+				box-shadow: 0 0 12px -6px var(--blue) inset;
+				transition: all 0.5s ease-in;
+			}
+
+			.keyboard-panel p:active {
+				background: var(--background-1);
+				text-shadow: 0 0 12px var(--blue);
+				transition: all 0.05s ease-in;
+			}
+		}`;
+		this.appendChild(styles);
 		this.build();
 	}
-	
+
 	build() {
 		let gridPanel = document.createElement('div');
 		gridPanel.setAttribute('class', 'keyboard-panel');
 		gridPanel.style.gridTemplateColumns = '1fr 1fr 1fr';
-		gridPanel.style.columnGap  = '1rem';
-		gridPanel.style.rowGap  = '1rem';
+		gridPanel.style.columnGap = '1rem';
+		gridPanel.style.rowGap = '1rem';
 
 		let buttons = this.getAttribute('buttons').split(' ');
 		for (let txt of buttons) {
-			let btn = document.createElement('h1');
+			let btn = document.createElement('p');
 			btn.style.textAlign = 'center';
 			btn.textContent = txt;
+			btn.onclick = (event) => btn.dispatchEvent(this.keyboardPressEvent);
+
 			gridPanel.appendChild(btn);
 		}
 
 		this.appendChild(gridPanel);
-
 	}
 }
 
@@ -32,61 +89,6 @@ class XKeyboard extends HTMLElement {
 
 (function init() {
 	window.customElements.define('x-keyboard', XKeyboard);
+	// character pressed is in target.textContent
+	// document.addEventListener('x-keyboard-press', (event) => console.log(event.target.textContent));
 })();
-
-
-/*
-.keyboard-panel {
-	display: grid;
-	width: 100%;
-	grid-template-columns: 1fr 1fr 1fr;
-	background: var(--background-0);
-	grid-column-gap: 1rem;
-	grid-row-gap: 1rem;
-}
-
-.keyboard-panel h1 {
-	user-select: none;
-	font-size: var(--xx-lg);
-	padding: 2rem;
-	transition: box-shadow 0.5s ease-out;
-}
-
-.keyboard-panel h1:hover {
-	box-shadow: 0 0 12px -6px var(--blue) inset;
-	transition: all 0.5s ease-in;
-}
-
-.keyboard-panel h1:active {
-	background: var(--background-1);
-	text-shadow: 0 0 12px var(--blue);
-	transition: all 0.05s ease-in;
-}
-
-@media screen and (max-width: 600px) {
-	.keyboard-panel {
-		grid-template-columns: 1fr 1fr 1fr;
-		background: var(--background-0);
-		grid-column-gap: 0.25rem;
-		grid-row-gap: 0.25rem;
-	}
-
-	.keyboard-panel h1 {
-		user-select: none;
-		font-size: var(--x-lg);
-		padding: 1rem;
-		transition: box-shadow 0.5s ease-out;
-	}
-
-	.keyboard-panel h1:hover {
-		box-shadow: 0 0 12px -6px var(--blue) inset;
-		transition: all 0.5s ease-in;
-	}
-
-	.keyboard-panel h1:active {
-		background: var(--background-1);
-		text-shadow: 0 0 12px var(--blue);
-		transition: all 0.05s ease-in;
-	}
-}
-*/
