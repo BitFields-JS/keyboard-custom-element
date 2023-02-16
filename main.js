9171aefa01c382e37049ba7ccaf9737b7b62ac6d
AES <josiphaboic@gmail.com>
2023-02-16 23:28:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,57 +7,56 @@ class XKeyboard extends HTMLElement {
 		this.keyboardPressEvent = new Event('x-keyboard-press', { bubbles: true, cancelable: false });
 		let styles = document.createElement('style');
 		styles.textContent = `
-		.keyboard-panel {
-			display: grid;
-			width: 100%;
-			grid-template-columns: 1fr 1fr 1fr;
-			background: var(--background-0);
-			grid-column-gap: 1rem;
-			grid-row-gap: 1rem;
+		x-keyboard[locked] {
+			color: #ff5500;
 		}
 
+		x-keyboard {
+			color: #00ffaa;
+		}
+
+		.keyboard-panel {
+			display: grid;
+			padding: 2rem;
+			grid-template-columns: 1fr 1fr 1fr;
+			grid-column-gap: 0.25rem;
+			grid-row-gap: 0.25rem;
+			background: #000000;
+
+		}
+		
 		.keyboard-panel p {
 			user-select: none;
-			font-size: var(--x-lg);
+			font-size: 2rem;
 			padding: 2rem;
-			transition: box-shadow 0.5s ease-out;
+			width: 5vw;
+			height: 5vw;
+			border-radius: ${this.hasAttribute('round') ? '50%' : '0%'  };
+			background: #000000;
 		}
 
 		.keyboard-panel p:hover {
-			box-shadow: 0 0 12px -6px var(--blue) inset;
-			transition: all 0.5s ease-in;
+			background: #000000;
+			transition: all 0.25s ease-in;
 		}
 
 		.keyboard-panel p:active {
-			background: var(--background-1);
-			text-shadow: 0 0 12px var(--blue);
+			color: #ffffff;
+			text-shadow: 0 0 12px #112211;
 			transition: all 0.05s ease-in;
 		}
 
 		@media screen and (max-width: 600px) {
 			.keyboard-panel {
-				grid-template-columns: 1fr 1fr 1fr;
-				background: var(--background-0);
-				grid-column-gap: 0.25rem;
-				grid-row-gap: 0.25rem;
+				grid-column-gap: 0.1rem;
+				grid-row-gap: 0.1rem;
 			}
 		
 			.keyboard-panel p {
-				user-select: none;
-				font-size: var(--x-lg);
+				font-size: 2rem;
+				width: 64px;
+				height: 64px;
 				padding: 1rem;
-				transition: box-shadow 0.5s ease-out;
-			}
-		
-			.keyboard-panel p:hover {
-				box-shadow: 0 0 12px -6px var(--blue) inset;
-				transition: all 0.5s ease-in;
-			}
-
-			.keyboard-panel p:active {
-				background: var(--background-1);
-				text-shadow: 0 0 12px var(--blue);
-				transition: all 0.05s ease-in;
 			}
 		}`;
 		this.appendChild(styles);
@@ -89,6 +88,18 @@ class XKeyboard extends HTMLElement {
 
 (function init() {
 	window.customElements.define('x-keyboard', XKeyboard);
-	// character pressed is in target.textContent
-	// document.addEventListener('x-keyboard-press', (event) => console.log(event.target.textContent));
 })();
+
+
+// function example() {
+// 	// character pressed is in target.textContent
+// 	document.addEventListener('x-keyboard-press', (event) => console.log(event.target.textContent));
+// 	setInterval(() => {
+// 		let kbd0 = document.getElementById('keyboard-0');
+// 		if (kbd0.hasAttribute('locked')) {
+// 			kbd0.removeAttribute('locked');
+// 		} else {
+// 			kbd0.setAttribute('locked', true);
+// 		}
+// 	}, 5000);
+// }
